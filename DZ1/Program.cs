9164506420dfcf2de4645a01285bc658ebca02ad
Дмитрий Рябинцев <liil.murda.reiiip1@gmail.com>
2024-04-04 22:03:24 +0300

    + 1 DZ

--- DZ1/Program.cs
+++ DZ1/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int m = 1;
+        int n = 10;
+        Console.WriteLine("Все натуральные числа в промежутке от " + m + " до " + n + ":");
+        PrintNumbers(m, n);
+    }
+
+    static void PrintNumbers(int m, int n)
+    {
+        if (m > n)
+        {
+            return;
+        }
+        Console.Write(m + " ");
+        PrintNumbers(m + 1, n);
+    }
+}
