0f351a9f8bc8a31df7bb8ee72dab34416b214ee8
Дмитрий Рябинцев <liil.murda.reiiip1@gmail.com>
2024-04-04 22:27:37 +0300

    DZ2 +

---
+++ DZ2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int m = 3;
+        int n = 2;
+        Console.WriteLine("Функция Аккермана для m = " + m + " и n = " + n + " равняется: " + Ackermann(m, n));
+    }
+
+    static int Ackermann(int m, int n)
+    {
+        if (m == 0)
+        {
+            return n + 1;
+        }
+        else if (n == 0)
+        {
+            return Ackermann(m - 1, 1);
+        }
+        else
+        {
+            return Ackermann(m - 1, Ackermann(m, n - 1));
+        }
+    }
+}
